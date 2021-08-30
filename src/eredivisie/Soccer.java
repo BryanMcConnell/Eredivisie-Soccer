86302622338261b5ed488d47c913a647c557dd1d
BryanMcConnell <McConnellBryan@protonmail.com>
2021-08-29 21:26:43 -0500

    added total excitement level between two teams

--- src/eredivisie/Soccer.java
+++ src/eredivisie/Soccer.java
@@ -21,10 +21,11 @@ public class Soccer {
 	// Team ranking by goals scored in current season based on array position
 		byte[] goalsRankingArray = {1,8,4,2,4,6,4,4,7,5,1,3,7,6,2,7,4,8};
 		
-	// An arbitrary excitement rating for two teams
+	// An arbitrary excitement rating for two teams & overall
 		
 		double excitementTeamOne = 0;
 		double excitementTeamTwo = 0;
+		double totalExcitement = 0;
 		
 		System.out.println("The teams in the Eredivisie are:");
 		for (int i = 0; i <teamsArray.length; i++)
@@ -46,22 +47,28 @@ public class Soccer {
 		System.out.println("You selected: " + teamsArray[teamSelectionTwo -1]);
 		System.out.println(teamsArray[teamSelectionTwo -1] + " was ranked " + rankingArray[teamSelectionTwo -1]);
 
-	// Assigns an excitement rating to each team
+	// Assigns an excitement rating to each team & overall
 		excitementTeamOne = (double)rankingArray[teamSelectionOne -1] + ((double)goalsRankingArray[teamSelectionOne -1] / 2);
 		excitementTeamTwo = (double)rankingArray[teamSelectionTwo -1] + ((double)goalsRankingArray[teamSelectionTwo -1] / 2);
-		
+		totalExcitement = excitementTeamOne + excitementTeamTwo;
 
 	// Compares the two teams
 		if (rankingArray[teamSelectionOne -1] > rankingArray[teamSelectionTwo -1])
 			System.out.println(teamsArray[teamSelectionTwo -1] +" was ranked better.");
-			else if (rankingArray[teamSelectionOne -1] < rankingArray[teamSelectionTwo -1])
-				System.out.println(teamsArray[teamSelectionOne - 1] +" was ranked better");
-			else
-				System.out.println("Error in selection");
-
-		System.out.println(teamsArray[teamSelectionOne -1] + " has an exitement rating of " + excitementTeamOne);
-		System.out.println(teamsArray[teamSelectionTwo -1] + " has an exitement rating of " + excitementTeamTwo);
-		System.out.println((double)rankingArray[teamSelectionOne -1] + " + " + ((double)goalsRankingArray[teamSelectionOne -1] / 2));
+		else if (rankingArray[teamSelectionOne -1] < rankingArray[teamSelectionTwo -1])
+			System.out.println(teamsArray[teamSelectionOne - 1] +" was ranked better");
+		else
+			System.out.println("Error in selection");
+		
+	// Display excitement level of a game between two teams
+		if (totalExcitement < 5)
+			System.out.println("This will be a very exciting game!");
+		else if (totalExcitement < 11)	
+			System.out.println("This will be an exciting game!");
+		else if (totalExcitement < 21)
+			System.out.println("This will be an interesting game.");
+		else
+			System.out.println("This might be a dull one.");
 
 
 	}
